95d66093f52b78c275ded5a9d5716b8c7544f2b2
Daniel Alejandro Sanchez Paez <programazion1@gmail.com>
2022-12-19 17:13:25 -0500

    First Commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string API_URL = "https://jsonplaceholder.typicode.com/posts";
+            var client = new WebClient();
+            var json = client.DownloadString(API_URL);
+            dynamic posts = JsonConvert.DeserializeObject(json);
+
+            foreach( var post in posts) {
+                Console.WriteLine(post.id + ": " + post.title);
+            }
+        }
+    }
+}
